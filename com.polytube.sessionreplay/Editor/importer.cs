0f70f933cad9056d521fdfe78c85e447821e3e88
AmirSolt <soltamir194@gmail.com>
2025-10-06 13:13:20 -0400

    upd

--- com.polytube.sessionreplay/Editor/importer.cs
+++ com.polytube.sessionreplay/Editor/importer.cs
@@ -2,37 +2,40 @@ using System.IO;
 using UnityEditor;
 using UnityEngine;
 
-[InitializeOnLoad]
-public static class FFmpegFileEnsurer
+namespace Polytube.SessionReplay
 {
-    static FFmpegFileEnsurer()
+    [InitializeOnLoad]
+    public static class FFmpegFileEnsurer
     {
-        CopyFFmpeg();
-    }
-
-    private static void CopyFFmpeg()
-    {
-        string sourcePath = "Packages/com.polytube.sessionreplay/Editor/assets/replay.exe";
-        string destDir = Path.Combine(Application.streamingAssetsPath, "com.polytube.sessionreplay");
-        string destPath = Path.Combine(destDir, "replay.exe");
-
-        if (!Directory.Exists(Application.streamingAssetsPath))
-            Directory.CreateDirectory(Application.streamingAssetsPath);
-
-        if (!Directory.Exists(destDir))
-            Directory.CreateDirectory(destDir);
-
-        if (File.Exists(sourcePath))
+        static FFmpegFileEnsurer()
         {
-            if (!File.Exists(destPath)) // copy only if missing
-            {
-                File.Copy(sourcePath, destPath, overwrite: true);
-                Debug.Log($"[ReplayFileEnsurer] Copied {sourcePath} -> {destPath}. This file is needed for SessionReplay Package!");
-            }
+            CopyFFmpeg();
         }
-        else
+
+        private static void CopyFFmpeg()
         {
-            Debug.LogWarning($"[ReplayFileEnsurer] Source file not found: {sourcePath}. This file is needed for SessionReplay Package!");
+            string sourcePath = "Packages/com.polytube.sessionreplay/Editor/assets/replay.exe";
+            string destDir = Path.Combine(Application.streamingAssetsPath, "com.polytube.sessionreplay");
+            string destPath = Path.Combine(destDir, "replay.exe");
+
+            if (!Directory.Exists(Application.streamingAssetsPath))
+                Directory.CreateDirectory(Application.streamingAssetsPath);
+
+            if (!Directory.Exists(destDir))
+                Directory.CreateDirectory(destDir);
+
+            if (File.Exists(sourcePath))
+            {
+                if (!File.Exists(destPath)) // copy only if missing
+                {
+                    File.Copy(sourcePath, destPath, overwrite: true);
+                    Debug.Log($"[ReplayFileEnsurer] Copied {sourcePath} -> {destPath}. This file is needed for SessionReplay Package!");
+                }
+            }
+            else
+            {
+                Debug.LogWarning($"[ReplayFileEnsurer] Source file not found: {sourcePath}. This file is needed for SessionReplay Package!");
+            }
         }
     }
 }
